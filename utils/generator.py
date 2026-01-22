af621035c638582441c9f7aa705ce3e95fef6eb8
요리하는개발자 <37555205+DevYeop@users.noreply.github.com>
2026-01-23 02:29:18 +0900

    feat : utils 코드 추가

---
+++ utils/generator.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+def generate_random_name(length=5):
+    """
+    테스트용 랜덤 문자열을 생성합니다 (예: 'abcde')
+    """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+def generate_test_email():
+    """
+    랜덤 이메일을 생성합니다 (예: 'abcde@test.com')
+    """
+    return f"{generate_random_name()}@test.com"
